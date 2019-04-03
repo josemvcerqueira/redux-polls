fe0072f7c3abd3c8b86ee0938c885f929ef1b005
Jose Cerqueira <jose@origensmedia.com>
2019-04-03 09:49:16 +0100

    feat: added middleware to our redux store - thunk and logger

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
+import middleware from "./middleware";
 import "./index.css";
 import App from "./components/App";
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
 	<Provider store={store}>
---
+++ src/middleware/index.js
@@ -0,0 +1,3 @@
+import logger from "./logger";
+import thunk from "redux-thunk";
+import { applyMiddleware } from "redux";
