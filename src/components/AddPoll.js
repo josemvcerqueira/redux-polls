0d4e9e42352df202967a7c2ef862ead95b30795b
Jose Cerqueira <jose@origensmedia.com>
2019-04-03 18:16:18 +0100

    feat: Added the action and reducer to update the redux state

--- src/components/AddPoll.js
+++ src/components/AddPoll.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleAddPoll } from "../actions/polls";
 
 class AddPoll extends Component {
 	state = {
@@ -27,6 +29,7 @@ class AddPoll extends Component {
 		event.preventDefault();
 		// Redirect to /
 		console.log("Add Poll:", this.state);
+		this.props.dispatch(handleAddPoll(this.state));
 	};
 
 	render() {
@@ -98,4 +101,4 @@ class AddPoll extends Component {
 	}
 }
 
-export default AddPoll;
+export default connect()(AddPoll);
